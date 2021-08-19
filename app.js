707b03ce4dac950136d12b6d72b25b8c528ad485
Blaise Gerstung <info@clearfeather.com>
2021-08-19 16:03:03 -0500

    updates

---
+++ app.js
@@ -0,0 +1,9 @@
+// Select everything
+
+// Player One Score number & button
+const p1Score = document.querySelector('#p1Score');
+const p1Button = document.querySelector('#p1Button');
+
+// Player 2 Score number & button
+const p2Score = document.querySelector('#p2Score');
+const p2Button = document.querySelector('#p2Button');

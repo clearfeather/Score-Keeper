47b9daf182c819e1613768c243461d6afaa7f2a6
clearfeather <info@clearfeather.com>
2022-04-28 08:56:43 -0500

    Update App.js

--- app.js
+++ app.js
@@ -8,16 +8,23 @@ const p1Button = document.querySelector('#p1Button');
 const p2Display = document.querySelector('#p2Display');
 const p2Button = document.querySelector('#p2Button');
 
+// Initiate Each player score
 let p1Score = 0;
 let p2Score = 0;
 
+// Winning Score
+let winningScore = 5;
 
+// Adding Points to Each Player
 p1Button.addEventListener('click', function() {
-    p1Score += 1;
-    p1Display.textContent = p1Score;
+    if(p1Score !== winningScore) {
+        p1Score += 1;
+        p1Display.textContent = p1Score;
+    }
 })
 p2Button.addEventListener('click', function() {
-    p2Score += 1;
-    p2Display.textContent = p2Score;
+    if(p2Score !== winningScore) {
+        p2Score += 1;
+        p2Display.textContent = p2Score;
+    }
 })
-

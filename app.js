f0fb3447aa685256315fb632ed55ff8b1ace5cf4
Clearfeather <info@clearfeather.com>
2022-05-02 16:43:35 -0500

    update app js and formatting

--- app.js
+++ app.js
@@ -19,28 +19,34 @@ let p2Score = 0;
 const gameType = document.querySelector('#gameType');
 winningScore = 3;
 
+// Start game off active (not game over)
 let isGameOver = false;
 
-// Adding Points to Each Player
+// Player One
 p1Button.addEventListener('click', function () {
     if (!isGameOver) {
         p1Score += 1;
         if (p1Score === winningScore) {
             isGameOver = true;
-            p1Display.classList.add('winner');
-            p2Display.classList.add('loser');
+            p1Display.classList.add('has-text-success');
+            p2Display.classList.add('has-text-danger');
+            p1Button.disabled = true;
+            p2Button.disabled = true;
         }
         p1Display.textContent = p1Score;
     }
 })
 
+// Player Two
 p2Button.addEventListener('click', function () {
     if (!isGameOver) {
         p2Score += 1;
         if (p2Score === winningScore) {
             isGameOver = true;
-            p2Display.classList.add('winner');
-            p1Display.classList.add('loser');
+            p2Display.classList.add('has-text-success');
+            p1Display.classList.add('has-text-danger');
+            p1Button.disabled = true;
+            p2Button.disabled = true;
         }
         p2Display.textContent = p2Score;
     }
@@ -60,6 +66,8 @@ function reset() {
     p2Score = 0;
     p1Display.textContent = 0;
     p2Display.textContent = 0;
-    p1Display.classList.remove('winner', 'loser');
-    p2Display.classList.remove('winner', 'loser');
+    p1Display.classList.remove('has-text-success', 'has-text-danger');
+    p2Display.classList.remove('has-text-success', 'has-text-danger');
+    p1Button.disabled = false;
+    p2Button.disabled = false;
 }
